c702056ad511e4e0042e9e6ad75509573fd4949e
shubham-deshmukh <shubham.deshmukh841@gmail.com>
2025-11-25 11:29:31 +0530

    Fetch real resumes on the home page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,45 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
   resume: Resume;
 }) => {
+  const { auth, fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      const url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+    loadResume();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
       className="resume-card animate-in fade-in duration-1000"
     >
-      
-        {/* Resume Card Header */}
-        <div className="resume-card-header">
-          {/* Job Details */}
-          <div className="flex flex-col gap-2">
+      {/* Resume Card Header */}
+      <div className="resume-card-header">
+        {/* Job Details */}
+        <div className="flex flex-col gap-2">
+          {companyName && (
             <h2 className="text-black! text-bold wrap-break-words">
               {companyName}
             </h2>
+          )}
+          {jobTitle && (
             <h3 className="text-gray-500 text-lg wrap-break-word">
               {jobTitle}
             </h3>
-          </div>
-          {/* Score Meter : circle */}
-          <div className="shrink-0">
-            <ScoreCircle score={feedback.overallScore} />
-          </div>
+          )}
+          {!companyName && !jobTitle && (
+            <h2 className="text-black! font-bold">Resume</h2>
+          )}
         </div>
-        {/* Resume Image */}
+        {/* Score Meter : circle */}
+        <div className="shrink-0">
+          <ScoreCircle score={feedback.overallScore} />
+        </div>
+      </div>
+      {/* Resume Image */}
+      {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
           <div className="w-full h-full">
             <img
-              src={imagePath}
+              src={resumeUrl}
               alt="resume"
               className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
             />
           </div>
         </div>
-      
+      )}
     </Link>
   );
 };
